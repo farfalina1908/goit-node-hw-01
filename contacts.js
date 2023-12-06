52ed4492447506dc913ea0266083c66437d985f5
Elena Berezina <farfalina1908@gmail.com>
2023-12-06 17:29:58 +0200

    add function in contact.js

--- contacts.js
+++ contacts.js
@@ -5,27 +5,56 @@ const { nanoid } = require("nanoid")
 const contactsPath = path.join(__dirname, "db", "contacts.json")
 
 // TODO: задокументировать каждую функцию
-function listContacts() {
-   return
+async function listContacts() {
+   const data = await fs.readFile(contactsPath)
+   const contacts = JSON.parse(data)
+   // console.log(contacts)
+   return contacts
+
    // ...твой код. Возвращает массив контактов.
 }
 
-function getContactById(contactId) {
-   if (contactId !== id) {
+async function getContactById(contactId) {
+   const contacts = await listContacts()
+   const result = contacts.find((item) => contactId === item.id)
+   if (!result) {
       return null
    }
-   return { contactId }
+   // console.log(result)
+   return { result }
+
    // ...твой код. Возвращает объект контакта с таким id. Возвращает null, если объект с таким id не найден.
 }
 
-function removeContact(contactId) {
+async function removeContact(contactId) {
+   const contacts = await listContacts()
+   const idx = contacts.findIndex((item) => contactId === item.id)
+
+   if (idx === -1) {
+      return null
+   }
+   const newContacts = contacts.filter((_, index) => index !== idx)
+
+   await fs.writeFile(contactsPath, JSON.stringify(newContacts))
+
+   console.log(contacts[idx])
+
+   return contacts[idx]
    // ...твой код. Возвращает объект удаленного контакта. Возвращает null, если объект с таким id не найден.
 }
 
-function addContact(name, email, phone) {
+async function addContact(name, email, phone) {
+   const contacts = await listContacts()
+   const newContact = { id: nanoid(), name, email, phone }
+   contacts.push(newContact)
+   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+
+   console.log(newContact)
+
+   return newContact
    // ...твой код. Возвращает объект добавленного контакта.
 }
-module.export = {
+module.exports = {
    listContacts,
    getContactById,
    removeContact,
---
+++ index.js
@@ -0,0 +1,6 @@
+const contactsHandling = require("./contacts")
+
+// contactsHandling.listContacts()
+// contactsHandling.getContactById("qdggE76Jtbfd9eWJHrssH")
+// contactsHandling.removeContact("vza2RIzNGIwutCVCs4mCL")
+contactsHandling.addContact("Elena", "farfalina@gmail.com", "+380504507923")
